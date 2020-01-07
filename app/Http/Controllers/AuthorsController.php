4e68b4250ad7a5f550b4fc60de7e8c7a6c0d07f5
bogdan demchenko <bogdandemchenko@gmail.com>
2020-01-07 17:38:09 +0200

    add author resource

---
+++ app/Http/Controllers/AuthorsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Author;
+use Illuminate\Http\Request;
+
+class AuthorsController extends Controller
+{
+    public function store()
+    {
+        $author = Author::create(request()->only([
+            'name', 'dob'
+        ]));
+
+//        return redirect($author->path());
+    }
+}
